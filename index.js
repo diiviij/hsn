82d098b1df4a9748fca0637c68b9fc4da2c9d7a3
diiviij <68986989+diiviij@users.noreply.github.com>
2020-09-23 16:41:49 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+<script>
+function allowDrop(event) {
+  event.preventDefault();
+}function drag(event) {
+  event.dataTransfer.setData("text", event.target.id);
+}function drop(event) {
+  event.preventDefault();
+  var data = event.dataTransfer.getData("text");
+  event.target.appendChild(document.getElementById(data));
+}
+
+  </script>
