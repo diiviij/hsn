eafa563383a43ed652037410d043b1a6197f7533
diiviij <68986989+diiviij@users.noreply.github.com>
2020-09-29 19:19:35 +0000

    Update o.js

--- o.js
+++ o.js
@@ -127,5 +127,43 @@
         // Create a layer manager for controlling layer visibility.
         var layerManager = new LayerManager(wwd);
         }
+ // ---------------------------------------
+                // Define the view model for the SearchBox
+                // ---------------------------------------
+                function SearchViewModel(wwd) {
+                    var self = this;
+                    self.geocoder = new WorldWind.NominatimGeocoder();
+                    self.goToAnimator = new WorldWind.GoToAnimator(wwd);
+                    self.searchText = ko.observable('');
+                    self.onEnter = function (data, event) {
+                        if (event.keyCode === 13) {
+                            self.performSearch();
+                        }
+                        return true;
+                    };
+                    self.performSearch = function () {
+                        var queryString = self.searchText();
+                        if (queryString) {
+                            var latitude, longitude;
+                            if (queryString.match(WorldWind.WWUtil.latLonRegex)) {
+                                var tokens = queryString.split(",");
+                                latitude = parseFloat(tokens[0]);
+                                longitude = parseFloat(tokens[1]);
+                                self.goToAnimator.goTo(new WorldWind.Location(latitude, longitude));
+                            } else {
+                                self.geocoder.lookup(queryString, function (geocoder, result) {
+                                    if (result.length > 0) {
+                                        latitude = parseFloat(result[0].lat);
+                                        longitude = parseFloat(result[0].lon);
+                                        self.goToAnimator.goTo(new WorldWind.Location(latitude, longitude));
+                                    }
+                                });
+                            }
+                        }
+                    };
+                }
+  ko.applyBindings(new SearchViewModel(wwd), document.getElementById('search'));
+
+
 
        
